c2e9e3c383df1a8197a90ee051609ac3a1a80040
Mike Chen <517926804@qq.com>
2020-12-30 14:16:25 +0800

    Update mimeapps.list

--- install.sh
+++ install.sh
@@ -57,7 +57,7 @@ if [ "$(get_os)" != "macos" ]; then
     ln -snf "$DOTFILES"/gitconfig_linux "$HOME"/.gitconfig
     ln -snf "$DOTFILES"/gtkrc-2.0 "$HOME"/.gtkrc-2.0
     ln -snf "$DOTFILES"/config/gtk-3.0 "$HOME"/.config/gtk-3.0
-    ln -snf "$DOTFILES"/config/mimeapps.list "$HOME"/.config/mimeapps.list
+    cp -rf "$DOTFILES"/config/mimeapps.list "$HOME"/.config/mimeapps.list
     ln -snf "$DOTFILES"/config/libfm "$HOME"/.config/libfm
     ln -snf "$DOTFILES"/config/fontconfig "$HOME"/.config/fontconfig
     mkdir -p "$HOME"/.config/alacritty
