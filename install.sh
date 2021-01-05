e710cbeac1a87c01c96ec4c4e27772c46dc25753
Mike Chen <517926804@qq.com>
2021-01-05 19:39:26 +0800

    Add pcmanfm config

--- install.sh
+++ install.sh
@@ -59,6 +59,7 @@ if [ "$(get_os)" != "macos" ]; then
     cp -rf "$DOTFILES"/config/gtk-3.0/* "$HOME"/.config/gtk-3.0
     cp -rf "$DOTFILES"/config/mimeapps.list "$HOME"/.config/mimeapps.list
     ln -snf "$DOTFILES"/config/libfm "$HOME"/.config/libfm
+    ln -nsf "$DOTFILES"/config/pcmanfm "$HOME"/.config/pcmanfm
     ln -snf "$DOTFILES"/config/fontconfig "$HOME"/.config/fontconfig
     mkdir -p "$HOME"/.config/alacritty
     ln -snf "$DOTFILES"/config/alacritty/alacritty_linux.yml "$HOME"/.config/alacritty/alacritty.yml
