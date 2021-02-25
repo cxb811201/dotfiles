8338a6a5cb603998c7a69530768ca5da9af02913
Mike Chen <517926804@qq.com>
2021-02-25 16:06:53 +0800

    add dwm autostart

---
+++ config/dwm/autostart.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+setsid -f xsetroot -cursor_name left_ptr
+
+if ! pgrep -u $UID -x dunst > /dev/null; then
+    setsid -f dunst
+fi
+
+if ! pgrep -u $UID -x picom > /dev/null; then
+    setsid -f picom --config ~/.config/picom.conf
+fi
+
+if ! pgrep -u $UID -x dwmblocks > /dev/null; then
+    setsid -f dwmblocks
+fi
+
+setsid -f nitrogen --restore
+
+if ! pgrep -u $UID -x fcitx5 > /dev/null; then
+    setsid -f fcitx5
+fi
+
+if ! pgrep -u $UID -x qv2ray > /dev/null; then
+    sleep 1
+
+    if pgrep -u $UID -x xray > /dev/null; then
+        killall -q xray
+    fi
+
+    while pgrep -u $UID -x xray >/dev/null; do sleep 1; done
+
+    setsid -f qv2ray
+fi
