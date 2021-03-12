559d2df4e2234f1f2e2fed001ca23d0b93467b36
Mike Chen <517926804@qq.com>
2021-03-12 15:53:51 +0800

    Add powermenu.sh

---
+++ local/bin/powermenu.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+declare -a options=(
+    "Lock screen"
+    "Logout"
+    "Reboot"
+    "Shutdown"
+)
+
+yesno(){
+    # shellcheck disable=SC2005
+    echo "$(echo -e "No\nYes" | dmenu -c -l 2 -i -p "${1}")"
+}
+
+# Piping the above array into dmenu.
+# We use "printf '%s\n'" to format the array one item to a line.
+choice=$(printf '%s\n' "${options[@]}" | dmenu -c -l 4 -i -p 'Power menu:')
+
+# What to do when/if we choose one of the options.
+case $choice in
+    'Logout')
+        if [[ $(yesno "Logout?") == "Yes" ]]; then
+            dwmc quit 0
+        fi
+        ;;
+    'Lock screen')
+        betterlockscreen -l
+        ;;
+    'Reboot')
+        if [[ $(yesno "Reboot?") == "Yes" ]]; then
+            systemctl reboot
+        fi
+        ;;
+    'Shutdown')
+        if [[ $(yesno "Shutdown?") == "Yes" ]]; then
+            systemctl poweroff
+        fi
+        ;;
+esac
